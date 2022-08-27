4526dae08e046e3221fac19b3826da10ac1da115
Lucas <almeidalucas1802@gmail.com>
2022-08-27 19:23:01 -0300

    'respfix'

--- Display/BikeUtils.py
+++ Display/BikeUtils.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib as mpl
 import seaborn
 import plotly.express as px
-from plotly.offline import plot
 class BikeAnalyze():
   """
   Define Ferramentas de Analise de treinos de ciclismo.
@@ -94,7 +93,7 @@ class BikeAnalyze():
                    xaxis_title='Tempo',
                    yaxis_title='Watts')
 
-    return plot(fig, output_type='div', include_plotlyjs=False)
+    return fig.to_html()
 
   def grafico_cadencia(self):
     '''
@@ -104,7 +103,7 @@ class BikeAnalyze():
     fig.update_layout(title='Gráfico de Cadência',
                    xaxis_title='Tempo',
                    yaxis_title='RPM')
-    return plot(fig, output_type='div', include_plotlyjs=False)
+    return fig.to_html()
 
   def grafico_zonas(self):
     '''
@@ -117,7 +116,7 @@ class BikeAnalyze():
     fig.update_traces(textposition='inside', textinfo='percent+label',hoverinfo='label+percent',
                      marker=dict(colors=colors, line=dict(color='#000000', width=2)))
 
-    return plot(fig, output_type='div', include_plotlyjs=False)
+    return fig.to_html()
 
   def gerar_relatorio(self):
 
